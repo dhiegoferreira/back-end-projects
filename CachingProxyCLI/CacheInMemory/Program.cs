c34d8477bf680cbd7527fe0b7ed4ca7cfb3da5a5
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2024-12-10 21:26:53 +0000

    CP: CacheInMemory

---
+++ CachingProxyCLI/CacheInMemory/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.Extensions.Caching.Memory;
+
+static void CacheInMemory()
+{
+    MemoryCache cache = new MemoryCache(new MemoryCacheOptions());
+    string key = "sampleKey";
+    string value = "Hello, Cache!";
+
+    cache.Set(key, value);
+    cache.Set()
+
+    if (cache.TryGetValue(key, out string cachedValue))
+    {
+        Console.WriteLine($"Cache hit: {cachedValue}");
+    }
+    else
+    {
+        Console.WriteLine("Cache miss.");
+    }
+}
+
+
+CacheInMemory();
