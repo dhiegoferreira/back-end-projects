f528f91f3717dc9de3aded90886e8e39e15682ec
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2024-12-10 21:26:31 +0000

    CP: HttpRequest

---
+++ CachingProxyCLI/HttpRequest/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+static async Task HttpRequest()
+{
+    HttpClient client = new HttpClient();
+    string url = "https://jsonplaceholder.typicode.com/todos/1";
+
+    HttpResponseMessage response = await client.GetAsync(url);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine("Response from server:");
+        Console.WriteLine(responseBody);
+
+}
+
+await HttpRequest();
